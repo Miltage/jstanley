4d5299dbdc1703c905dcccf42b72bc145e87dfef
Robert Shenton <theshenton@gmail.com>
2019-11-15 00:10:00 +0200

    Add jazz bar scene

--- src/Scene.js
+++ src/Scene.js
@@ -1,12 +1,24 @@
 import * as PIXI from 'pixi.js';
 
+let bg = null;
+
 export default class Scene extends PIXI.Container {
 
   constructor(props) {
     super(props);
 
+    bg = PIXI.Sprite.from('jazzbar.jpg');
+    this.addChild(bg);
+
     let cat = PIXI.Sprite.from('cat.jpg');
     this.addChild(cat);
+  }
+
+  resize() {
+    var ratio = window.innerWidth / bg.width;
+    console.log(bg.width);
+    bg.scale.set(1, 1);
+    console.log(ratio);
   }
 
 }
--- src/index.js
+++ src/index.js
@@ -10,73 +10,28 @@ var song = new Howl({
 
 //song.play();
 
-const logicalWidth = 320;
-const logicalHeight = 240;
-let scaleFactor = 0;
-let renderer = null;
-let stage = null;
+const app = new PIXI.Application({ resizeTo: window });
 
-const animate = () => {
-  // start the timer for the next animation loop
-  requestAnimationFrame(animate);
+let scene = new Scene();
 
-  renderer.render(stage);
-};
+app.stage.addChild(scene);
 
-const resizeHandler = () => {
-  scaleFactor = Math.floor(Math.min(
+// The application will create a canvas element for you that you
+// can then insert into the DOM
+document.body.appendChild(app.view);
+
+const logicalWidth = 1280;
+const logicalHeight = 720;
+
+const resize = () => {
+  var scaleFactor = Math.min(
     window.innerWidth / logicalWidth,
     window.innerHeight / logicalHeight
-  ));
-  const newWidth = Math.ceil(logicalWidth * (window.innerWidth / logicalWidth));
-  const newHeight = Math.ceil(logicalHeight * (window.innerHeight / logicalHeight));
-  
-  renderer.view.style.width = `${newWidth}px`;
-  renderer.view.style.height = `${newHeight}px`;
+  );
 
-  renderer.resize(newWidth, newHeight);
+  app.stage.scale.set(scaleFactor);
+  scene.resize();
 };
 
-const init = () => {
-  renderer = PIXI.autoDetectRenderer(logicalWidth, logicalHeight, {
-    roundPixels: true,
-    resolution: window.devicePixelRatio || 1
-  });
-  renderer.view.id = 'pixi-canvas';
-
-  renderer.view.addEventListener('contextmenu', (e) => {
-    window.wasRightClick = true;
-    e.preventDefault();
-  });
-
-  PIXI.settings.ROUND_PIXELS = true;
-  PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
-  
-  stage = new PIXI.Container();
-
-  var scene = new Scene();
-  stage.addChild(scene);
-  
-  document.body.appendChild(renderer.view);
-  window.addEventListener('resize', resizeHandler, false);
-  resizeHandler();
-  
-  renderer.backgroundColor = 0xdefec8;
-
-  PIXI.Loader.shared.add("cat.jpg")
-    .on("progress", loadProgressHandler)
-    .load(setup);
-
-  function loadProgressHandler(loader, resource) {
-    //Display the file `url` currently being loaded
-    //console.log("loading: " + resource.url);
-    //Display the precentage of files currently loaded
-    //console.log("progress: " + loader.progress + "%"); 
-  }
-}
-
-const setup = () => {
-  animate();
-};
-
-init();
+window.addEventListener('resize', resize, false);
+resize();
