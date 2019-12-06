b7cf2e63cef956aa7f8c9b9341a34631ec5759d3
Robert Shenton <theshenton@gmail.com>
2019-12-06 12:13:35 +0200

    Change build folder

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist',
+    contentBase: './docs',
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -25,6 +25,6 @@ module.exports = {
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
 };
