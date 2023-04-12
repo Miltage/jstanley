5cb6624ddf7bca2664eb797a2b719e2fc2692acf
Robert Shenton <theshenton@gmail.com>
2023-04-12 09:17:06 +0200

    Clean out repo

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-npm run build;
-git add dist;
-git commit -m "Deploy site";
-git subtree push --prefix dist origin gh-pages;
--- src/ScreenSize.js
+++
@@ -1,9 +0,0 @@
-class ScreenSize {
-  constructor() {
-    this.width = window.innerWidth;
-    this.height = window.innerHeight;
-    this.rect = null;
-  }
-}
-
-export default (new ScreenSize);
